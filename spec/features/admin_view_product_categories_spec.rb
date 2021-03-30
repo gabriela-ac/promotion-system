67471b4c200e4f1fe9e02af62089a91eb1ef080d
Gabriela Castro <gabriela.allcastro@gmail.com>
2021-03-30 15:54:41 -0300

    WIP Outros testes

--- spec/features/admin_view_product_categories_spec.rb
+++ spec/features/admin_view_product_categories_spec.rb
@@ -12,5 +12,12 @@ feature 'Admin view product categories' do
     expect(page).to have_content('HOSP')
     expect(page).to have_content('E-mail')
     expect(page).to have_content('EMAIL')
+    expect(page).to have_link('Voltar', href: root_path)
+  end
+
+  xscenario 'and show empty message' do
+  end
+
+  xscenario 'and view details' do
   end
 end
