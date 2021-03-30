d5bf64b7f86f73902147b2547d88add5d30d9672
Gabriela Castro <gabriela.allcastro@gmail.com>
2021-03-30 12:53:38 -0300

    Criar promoção

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -6,4 +6,21 @@ class PromotionsController < ApplicationController
   def show
     @promotion = Promotion.find(params[:id])
   end
+
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    @promotion = Promotion.create(promotion_params)
+    redirect_to @promotion
+  end
+
+  private
+
+  def promotion_params
+    params
+      .require(:promotion)
+      .permit(:name, :description, :code, :discount_rate, :expiration_date, :coupon_quantity)
+  end
 end
