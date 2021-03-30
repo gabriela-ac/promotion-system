82eb140ec02d0d1e4660b076a4e51efe72262145
Gabriela Castro <gabriela.allcastro@gmail.com>
2021-03-30 15:47:46 -0300

    WIP Ver categorias de produto

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_183849) do
+ActiveRecord::Schema.define(version: 2021_03_30_182909) do
+
+  create_table "product_categories", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["code"], name: "index_product_categories_on_code", unique: true
+  end
 
   create_table "promotions", force: :cascade do |t|
     t.string "name"
